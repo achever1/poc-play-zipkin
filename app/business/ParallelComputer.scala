b93b27880a7376bb600aa96bf99e1d4e1b78a841
Armel Chever <achever@talend.com>
2018-08-17 11:28:00 +0200

    trace futures => inter threads data propagation (#1)

---
+++ app/business/ParallelComputer.scala
@@ -0,0 +1,26 @@
+package business
+
+import akka.actor.ActorSystem
+import javax.inject.Inject
+import jp.co.bizreach.trace.{TraceData, ZipkinTraceServiceLike}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class ParallelComputer @Inject()(
+    system: ActorSystem,
+    val tracer: ZipkinTraceServiceLike)(implicit ec: ExecutionContext) {
+
+  def compute(implicit parentTraceData: TraceData) =
+    Future
+      .sequence {
+        (1 to 5)
+          .map { i =>
+            tracer.traceFuture("compute locally") { _ =>
+              Future(i)
+                .map(_ * 10)
+                .map(_ + 1)
+            }
+          }
+      }
+      .map(_.sum)
+}
